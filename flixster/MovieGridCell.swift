097a350db8f6d845eeb73e28b298c22fc4b72611
lizparra <44618559+lizparra@users.noreply.github.com>
2020-06-21 11:48:54 -0400

    Added new superhero tab

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Liz Parra on 6/21/20.
+//  Copyright © 2020 lizparra. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixster/ViewController.swift
+++ flixster/ViewController.swift
@@ -56,14 +56,36 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
-        
+
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl+posterPath)
-        
+
         cell.posterView.af.setImage(withURL: posterUrl!)
         
         return cell
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        print("Loading details")
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
     }
 
 }
